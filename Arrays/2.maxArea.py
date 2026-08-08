0aa5b134ed803a58f0a07128ba9b8e7bc5c72497
Subin-2005 <subinsr.developer@gmail.com>
2026-08-08 21:33:47 +0530

    first commit

---
+++ Arrays/2.maxArea.py
@@ -0,0 +1,27 @@
+import os
+os.system('cls')
+
+def maxArea(height):
+
+    left = 0
+    right = len(height) - 1
+    max_water = 0
+
+    while left < right:
+        width = right - left
+        current_height = min(height[left], height[right])
+
+        area = width*current_height
+        max_water = max(max_water, area)
+
+        if height[left] < height[right]:
+            left+=1
+
+        else:
+            right-=1
+
+    return max_water
+
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+maxArea = maxArea(height)
+print(maxArea)
